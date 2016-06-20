954f226a9e9e7bcbc77fa43e5d53a18153ea507e
Dan Barnes <dbarnes1@paychex.com>
2016-06-20 13:32:03 -0400

    Invoke event handlers in default browser context.

--- src/contexts/DOMEvents.js
+++ src/contexts/DOMEvents.js
@@ -34,7 +34,7 @@ define(['lodash'], function(_) {
                 }
                 var parent = ContextManager.getCurrentContext(),
                     eventHandler = function handleEvent(e) {
-                        var method = handler.bind(null, e),
+                        var method = _.partial(handler, e),
                             childName = initialize.prettify(e.target, type);
                         return parent.fork(childName + ': ' + getFunctionName(handler), method);
                     };
